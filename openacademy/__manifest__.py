d34501a747f9a011d8044c6ad3a1bb7eb000266f
morgs68 <hayden.james.morgan@gmail.com>
2020-01-10 14:06:45 +0000

    My 8th Commit

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
     ],
